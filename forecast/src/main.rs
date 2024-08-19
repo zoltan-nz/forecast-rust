5cd3956768a28911618ccbcb6a50166013142e67
Zoltan <zdebre@gmail.com>
2024-08-19 07:18:32 -0400

    feat: show hello world on localhost:3000

--- forecast/src/main.rs
+++ forecast/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
